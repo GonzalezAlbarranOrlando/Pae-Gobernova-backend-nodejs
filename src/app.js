73169915d1ef1080a38572db498506ff9a7bf011
González Albarrán Orlando <ogonzaleza1@toluca.tecnm.mx>
2024-03-05 09:18:19 -0600

    install cors & restrict requests

--- src/app.js
+++ src/app.js
@@ -2,10 +2,19 @@ const express = require('express');
 const config = require('./config');
 //npm i morgan
 const morgan = require('morgan');
+//cors
+const cors = require('cors');
 //import rutes
 const rutes = require('./modules/rutes');
 
 const app = express();
+
+// Habilitar CORS para todas las solicitudes
+//app.use(cors());
+// Habilitar CORS para origen específico
+app.use(cors({
+    origin: 'http://localhost:8080',
+  }));
 
 //middleware
 app.use(morgan('dev'));
